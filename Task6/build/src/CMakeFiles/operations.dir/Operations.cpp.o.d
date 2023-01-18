7b58382cd9ccc5752d526f0000d10b9c58c6eca2
nonameguy11 <nonameguy21@mail.ru>
2023-01-18 16:52:44 +1000

    Task6

---
+++ Task6/build/src/CMakeFiles/operations.dir/Operations.cpp.o.d
@@ -0,0 +1,3 @@
+src/CMakeFiles/operations.dir/Operations.cpp.o: \
+ /home/jeka/OOP/Task6/src/Operations.cpp /usr/include/stdc-predef.h \
+ /home/jeka/OOP/Task6/src/../include/operations/Operations.h
