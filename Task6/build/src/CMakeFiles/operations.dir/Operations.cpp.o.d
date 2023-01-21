dea0d075d91e9b532943e8064a35686a0c6fa507
nonameguy11 <nonameguy21@mail.ru>
2023-01-21 20:50:00 +1000

    deleted build

--- Task6/build/src/CMakeFiles/operations.dir/Operations.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/operations.dir/Operations.cpp.o: \
- /home/jeka/OOP/Task6/src/Operations.cpp /usr/include/stdc-predef.h \
- /home/jeka/OOP/Task6/src/../include/operations/Operations.h
