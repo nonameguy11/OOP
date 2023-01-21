ba3f4640141499a22a79676d9cbb989d2cdc7331
nonameguy11 <nonameguy21@mail.ru>
2023-01-21 20:05:12 +1000

    deleted build

--- Task2/build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jeka/OOP/Task2/googletest
-# Build directory: /home/jeka/OOP/Task2/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
